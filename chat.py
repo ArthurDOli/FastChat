9ec72e58289666367f61d333750ba744fe07cb45
Arthur Duarte Oliveira <arthur.duarte.oli06@gmail.com>
2025-10-04 15:05:02 -0300

    Added usernames to the chat

--- chat.py
+++ chat.py
@@ -1,23 +1,25 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Request
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Request, Depends
+from security import get_current_user_from_websocket
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
 from ws_manager import manager
+from models import User
 
 chat_router = APIRouter(prefix='/chat', tags=['Chat'])
 templates = Jinja2Templates(directory='templates')
 
-@chat_router.get('/', tags=['Chat'])
-async def chat_page(request: Request):
+@chat_router.get('/')
+async def get_chat_page(request: Request):
     return templates.TemplateResponse('chat.html', context={"request": request})
 
-@chat_router.websocket('/ws/{client_id}')
-async def chat_websocket(websocket: WebSocket, client_id: int):
-    await manager.connect(websocket)
+@chat_router.websocket('/ws')
+async def chat_websocket(websocket: WebSocket, current_user: User = Depends(get_current_user_from_websocket)):
+    username = current_user.username
+    await manager.connect(websocket, username)
+    await manager.broadcast_to_others({"sender": "System", "message": f"Usuário '{username}' entrou no chat"}, username)
     try:
         while True:
-            data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            data = await websocket.receive_json()
+            await manager.broadcast({"sender": username, "message": data['text']})
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+        await manager.broadcast_to_others({"sender": "System", "message": f"Usuário '{username}' saiu do chat"}, username)
--- security.py
+++ security.py
@@ -1,11 +1,10 @@
 from datetime import datetime, timedelta, timezone
 from typing import Optional
 from jose import JWTError, jwt
-from passlib.context import CryptContext
 from dotenv import load_dotenv
 import os
 from fastapi.security import OAuth2PasswordBearer, HTTPBearer, HTTPAuthorizationCredentials
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, status, Query
 from sqlalchemy.orm import Session
 from database import getSession
 from models import User
@@ -13,20 +12,17 @@ from models import User
 load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
+REFRESH_SECRET_KEY = os.getenv('REFRESH_SECRET_KEY')
 ALGORITHM = os.getenv('ALGORITHM')
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
-REFRESH_SECRET_KEY = os.getenv('REFRESH_SECRET_KEY')
 REFRESH_TOKEN_EXPIRE_MINUTES = int(os.getenv('REFRESH_TOKEN_EXPIRE_MINUTES'))
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/login')
 refresh_scheme = HTTPBearer()
 
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+def create_access_token(data: dict):
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({'exp': expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
@@ -39,41 +35,59 @@ def create_refresh_token(data: dict):
     return encoded_jwt
 
 def get_current_user(token: str = Depends(oauth2_scheme), session: Session = Depends(getSession)):
-    """
-    Dependência para decodificar o token, validar e retornar o usuário atual
-    """
-    credential_exception = HTTPException(
+    credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Não foi possível validar as credenciais",
-        headers={"WWW-Authenticate": 'Bearer'}
+        headers={"WWW-Authenticate": "Bearer"},
     )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        email: str = payload.get('sub')
+        email: str = payload.get("sub")
         if email is None:
-            raise credential_exception
+            raise credentials_exception
     except JWTError:
-        raise credential_exception
+        raise credentials_exception
+    
     user = session.query(User).filter(User.email == email).first()
     if user is None:
-        raise credential_exception
+        raise credentials_exception
+    
     return user
 
 def get_user_from_refresh_token(token: HTTPAuthorizationCredentials = Depends(refresh_scheme), session: Session = Depends(getSession)):
     credentials = token.credentials
-    credential_exception = HTTPException(
+    credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Não foi possível validar as credenciais",
-        headers={"WWW-Authenticate": 'Bearer'}
+        detail="Não foi possível validar o refresh token",
+        headers={"WWW-Authenticate": "Bearer"},
     )
     try:
         payload = jwt.decode(credentials, REFRESH_SECRET_KEY, algorithms=[ALGORITHM])
         email: str = payload.get("sub")
         if email is None:
-            raise credential_exception
+            raise credentials_exception
     except JWTError:
-        raise credential_exception
+        raise credentials_exception
+    
     user = session.query(User).filter(User.email == email).first()
     if user is None:
-        raise credential_exception
+        raise credentials_exception
+    
+    return user
+
+async def get_current_user_from_websocket(token: str = Query(...), session: Session = Depends(getSession)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Não foi possível validar as credenciais do WebSocket",
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = session.query(User).filter(User.email == email).first()
+    if user is None:
+        raise credentials_exception
     return user
