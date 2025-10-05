03e47d9a7512513b9e0f9379d9879ff983f691d3
Arthur Duarte Oliveira <arthur.duarte.oli06@gmail.com>
2025-10-04 22:53:50 -0300

    Frontend added

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # uvicorn main:app --reload
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from auth import auth_router
 from chat import chat_router
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(auth_router)
 app.include_router(chat_router)
